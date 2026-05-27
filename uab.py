9772f6d607094aeeac2f54364e1815b5523bcef7
Antony <antony.programmer@gmail.com>
2026-05-27 06:59:16 -0400

    uab en ascii

---
+++ uab.py
@@ -0,0 +1,26 @@
+u = [
+    "U     U",
+    "U     U",
+    "U     U",
+    "U     U",
+    " UUUUU "
+]
+
+a = [
+    "   A   ",
+    "  A A  ",
+    " A___A ",
+    "A     A",
+    "A     A"
+]
+
+b = [
+    "BBBBBB ",
+    "B     B",
+    "BBBBBB ",
+    "B     B",
+    "BBBBBB "
+]
+
+for i in range(5):
+    print(u[i] + "   " + a[i] + "   " + b[i])
